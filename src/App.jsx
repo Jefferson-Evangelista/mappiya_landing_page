7fad514a11d66db3fcf6e0b25a1c93b7f1189d89
Jefferson Evangelista <evangelistaxiv14@gmail.com>
2023-11-28 13:44:02 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,40 @@
 import React from 'react'
 import styles from './style'
-import { Routes. Route } from 'react-router-dom'
-
-
-//pages
 import { Navbar, Hero, Deliveries, About, Restaurants, Rates, Location, CTA, Footer } from './components';
 
 const App = () => (
-  <Routes>
-    <div className='bg-white container max-w-full overflow-hidden'>
+  <div className='bg-white container max-w-full overflow-hidden'>
+    <div className={`${styles.marginX}`}>
+      <Navbar/>
+    </div>
+    <div className={`${styles.marginX} ${styles.marginY} `}>
+      <Hero />
+    </div>
 
-      
-        <div className={`${styles.marginX}`}>
-          <Route path="/" element={<Hero/>}/>
-        </div>
-      
-      <div className={`${styles.marginX} ${styles.marginY} `}>
-        <Hero />
-      </div>
+    <About />
 
-      <About />
+    <div className={`${styles.marginX} ${styles.marginY} `}>
+      <Deliveries />
+    </div>
 
-      <div className={`${styles.marginX} ${styles.marginY} `}>
-        <Deliveries />
-      </div>
-
-      <div className={`${styles.marginX} ${styles.marginY}`}>
-        <Restaurants />
-      </div>
-      <div className={`bg-dimWhite ${styles.paddingX}`}>
-        <Rates />
-        
-      </div>
-      <div className={`${styles.marginY}`}>
-        <Location />
-      </div>
-      
-      <CTA />
-      <div className={`${styles.marginX} ${styles.marginY} `}>
-        <Footer />
-      </div>
-
+    <div className={`${styles.marginX} ${styles.marginY}`}>
+      <Restaurants />
+    </div>
+    <div className={`bg-dimWhite ${styles.paddingX}`}>
+      <Rates />
       
     </div>
-  </Routes>
+    <div className={`${styles.marginY}`}>
+      <Location />
+    </div>
+    
+    <CTA />
+    <div className={`${styles.marginX} ${styles.marginY} `}>
+      <Footer />
+    </div>
+
+    
+  </div>
     
 )
 
